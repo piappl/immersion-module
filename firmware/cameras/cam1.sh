afa9ef0f01445ea7f335747f26551f8598a6ce33
Jan Fraś <jfras@piap.pl>
2016-12-14 16:54:07 +0100

    New files for video transmission

---
+++ firmware/cameras/cam1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gst-launch-1.0   -v -e v4l2src  device=/dev/v4l/by-id/usb-046d_HD_Pro_Webcam_C920_3AFA6F4F-video-index0  !   video/x-h264,width=1280,height=720,framerate=30/1 ! h264parse !    rtph264pay !  udpsink host=192.168.230.71 port=1234 
